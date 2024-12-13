e741a01fb5f17290190ab6e40f21adacf87c2c49
Nardy <nardy.allstars@gmail.com>
2024-12-13 18:08:08 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar"
+import ".styles/Navbar.css"
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,23 @@
+function Navbar() {
+    return(
+        <div className="navbar">
+        <a class="nav-links" href="#formation">
+            <h3>Formation</h3>
+        </a>
+        <a class="nav-links" href="#skills">
+            <h3>Skills</h3>
+        </a>
+        <a class="nav-links" href="#projects">
+            <h3>Projects</h3>
+        </a>
+        <a class="nav-links" href="#experience">
+            <h3>Experience</h3>
+        </a>
+        <a class="nav-links" href="#languajes">
+            <h3>Languages</h3>
+        </a>
+        </div>
+    )
+}
+
+export default Navbar;
