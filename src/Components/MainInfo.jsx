5da4abceca5dd37183cad0ed254b52a18d06e4a5
forsakenardy <nardy.allstars@gmail.com>
2025-03-24 00:32:02 +0100

    text animation added

--- src/Components/MainInfo.jsx
+++ src/Components/MainInfo.jsx
@@ -1,18 +1,58 @@
-import "../styles/MainInfo.css"
+import React, { useEffect } from "react";
+import "../styles/MainInfo.css";
+import Typed from "typed.js";
 
 function MainInfo() {
+
+    useEffect(() => {
+        const optionsH1 = {
+            strings: ["Arnaldo Mera"],
+            typeSpeed: 30,
+            startDelay: 1600, 
+            loop: false,
+            showCursor: false,
+            contentType: "html",
+        };
+
+        const optionsH2 = {
+            strings: ["Software Developer"],
+            typeSpeed: 25,
+            startDelay: 2100,
+            loop: false,
+            showCursor: false,
+            contentType: "html",
+        };
+
+        const optionsH3 = {
+            strings: [
+                "Hi! I’m Arnaldo, a Software developer<br>" +
+                "specialized in the use of JAVASCRIPT, C#, MONGODB, EXPRESS, REACT, NODEJS, HTML &amp; CSS.<br>" +
+                "I’m passionate about creating dynamic websites and robust web applications.<br>" +
+                "I thrive on creating intuitive and efficient user experiences, always adhering to best practices in development and design."
+            ],
+            typeSpeed: 1,
+            startDelay: 2800,
+            loop: false,
+            showCursor: false,
+            contentType: "html",
+        };
+
+        new Typed(".typed-h1", optionsH1);
+        new Typed(".typed-h2", optionsH2);
+        new Typed(".typed-h3", optionsH3);
+    }, []);
+
     return (
         <>
             <div className="Nombre">
-                <h1>Arnaldo Mera</h1>
-                <h2>Software Developer</h2>
+                <h1 className="typed-h1"></h1>
+                <h2 className="typed-h2"></h2>
             </div>
-            <div className="About"></div>
-            <div className="Container1"></div>
-            <div className="Container2"></div>
+            <div className="Container1">
+                <h3 className="typed-h3"></h3>
+            </div>
         </>
-
-    )
+    );
 }
 
 export default MainInfo;
